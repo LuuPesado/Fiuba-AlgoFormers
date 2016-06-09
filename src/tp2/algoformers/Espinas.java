9cdf3a9bdb88bd1e7a7208ee02537d634124439c
santitraverso <santi.rtraverso@gmail.com>
2016-06-09 12:17:59 -0300

    arreglo

--- src/tp2/algoformers/Espinas.java
+++ src/tp2/algoformers/Espinas.java
@@ -7,16 +7,16 @@ public class Espinas extends TerrenoTerrestre {
   
     @Override
     public void afectarHumanoide(Algoformer unAlgoformer) {
-        int daño = unAlgoformer.puntosDeVida();
-        daño = daño*5/100;
-        unAlgoformer.recibirDanio(daño);
+        int danio = unAlgoformer.puntosDeVida();
+        danio = danio*5/100;
+        unAlgoformer.recibirDanio(danio);
     }
 
     @Override
     public void afectarVehiculo(Algoformer unAlgoformer) {
-        int daño = unAlgoformer.puntosDeVida();
-        daño = daño*5/100;
-        unAlgoformer.recibirDanio(daño);
+        int danio = unAlgoformer.puntosDeVida();
+        danio = danio*5/100;
+        unAlgoformer.recibirDanio(danio);
     }
    
 }
